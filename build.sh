cb180bada12b639dafe658c5b5b861f46551f9f4
frank <frank@schlottmann-goedde.de>
2026-03-09 12:00:20 +0100

    examples

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@
 docker kill icecast-fsg
 docker rm -f icecast-fsg
 docker rmi -f icecast-fsg
+rm data/iceshake.fdb
 time docker compose -f docker-compose.yml -f docker-compose-build.yml build
 
 # log into docker container
